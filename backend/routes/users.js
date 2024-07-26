782506ecef4970ba9593c02d484dadfcbe0acbb2
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-07-26 11:40:25 +0200

    update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,18 +3,26 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 const { usersCollection } = require("../database/database");
 
-router.post("/register", (req, res) => {
+app.post("/users/register", (req, res) => {
   const { username, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 8);
 
-  usersCollection.insert(
-    { username, password: hashedPassword },
-    (err, user) => {
-      if (err) return res.status(500).send(err);
-      const token = jwt.sign({ id: user._id }, secretKey, { expiresIn: 86400 });
-      res.status(200).send({ auth: true, token });
-    }
-  );
+  usersCollection.findOne({ username }, (err, user) => {
+    if (err) return res.status(500).send(err);
+    if (user) return res.status(400).send({ message: "User already exists" });
+
+    const hashedPassword = bcrypt.hashSync(password, 8);
+
+    usersCollection.insert(
+      { username, password: hashedPassword },
+      (err, user) => {
+        if (err) return res.status(500).send(err);
+        const token = jwt.sign({ id: user._id }, secretKey, {
+          expiresIn: 3600,
+        });
+        res.status(200).send({ auth: true, token });
+      }
+    );
+  });
 });
 
 router.post("/login", (req, res) => {
